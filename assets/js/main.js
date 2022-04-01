5eadd6c036493597c1efa84ce48f2e0a020aa76c
driztea17 <dri.sin.rt20@dypatil.com>
2022-04-01 10:46:46 +0530

    bg

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Ninestars - v4.7.0
-* Template URL: https://bootstrapmade.com/ninestars-free-bootstrap-3-theme-for-creative/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
